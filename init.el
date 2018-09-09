2c3c216c8ee0f4d8420777cd18995d1fcafa40d5
Stevo Sn <stevo@Fenris.asgard>
2018-09-09 22:45:52 +0200

    added org-babel-load-file

--- init.el
+++ init.el
@@ -10,9 +10,13 @@
  '(package-selected-packages
    (quote
     (unfill yasnippet-classic-snippets yasnippet py-autopep8 company-jedi company magit))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
