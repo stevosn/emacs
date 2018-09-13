78e124df5aa26fd4291b8b218f47bbd3491033cf
steves <steve.simmert@uni-tuebingen.de>
2018-09-13 15:22:36 +0200

    added downloaded folder to emacs path

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,6 +24,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+;;(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "downloaded" user-emacs-directory))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
