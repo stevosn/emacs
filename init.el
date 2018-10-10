4e9f0779ba37c4001a0682c4a9f922c7baab32b3
steves <steve.simmert@uni-tuebingen.de>
2018-10-10 13:40:30 +0200

    selected packages have changes (automatically) and org-faces were added

--- init.el
+++ init.el
@@ -14,16 +14,18 @@
    [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (misterioso)))
  '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (unfill yasnippet-classic-snippets yasnippet py-autopep8 company-jedi company magit))))
+ '(package-selected-packages (quote (projectile avy company magit))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.15))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.05))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
