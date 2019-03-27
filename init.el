922b4a979494d5ce2706ee7fcdac4f1ce0710554
Steve Simmert <steve.simmert@mp-ndt.de>
2019-03-27 09:50:41 +0100

    changed custom-set-faces: org-date to dim-grey

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
    [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (misterioso)))
  '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/Documents/notes/work.org")))
+ ;;'(org-agenda-files (quote ("~/Documents/notes/work.org")))
  '(package-selected-packages (quote (smex company magit))))
 
 (custom-set-faces
@@ -22,6 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-date ((t (:foreground "dim gray" :underline "dim gray"))))
  '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.15))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
