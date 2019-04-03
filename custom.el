407018c4be85029081d4674a69dd4f686ad75e4b
Stevo Sn <github_stevosn@fremiste.de>
2019-04-03 21:43:45 +0200

    added custom.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages (quote (smex company magit))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-date ((t (:foreground "dim gray" :underline "dim gray"))))
+ '(org-level-1 ((t (:foreground "cyan" :height 1.35))))
+ '(org-level-2 ((t (:foreground "light goldenrod" :weight normal :height 1.2))))
+ '(org-level-3 ((t (:foreground "orange" :height 1.2))))
+ '(org-level-4 ((t (:foreground "OliveDrab3" :height 1.15))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+ '(org-special-keyword ((t (:foreground "dim gray")))))
--- init.el
+++ init.el
@@ -5,30 +5,8 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/Documents/notes/work.org")))
- '(package-selected-packages (quote (smex company magit))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-date ((t (:foreground "dim gray" :underline "dim gray"))))
- '(org-level-1 ((t (:foreground "cyan" :height 1.35))))
- '(org-level-2 ((t (:foreground "light goldenrod" :weight normal :height 1.2))))
- '(org-level-3 ((t (:foreground "orange" :height 1.2))))
- '(org-level-4 ((t (:foreground "OliveDrab3" :height 1.15))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
- '(org-special-keyword ((t (:foreground "dim gray")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
